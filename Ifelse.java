eaf4b88fb593fee94154ccc0b141524c514aab43
Santu Das <Santuds71@gmail.com>
2023-01-09 22:41:41 +0530

    Java

---
+++ Ifelse.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Ifelse{
+ public static void main(String[] args) {
+    int no; 
+    Scanner obj=new Scanner(System.in);
+     System.out.println("Enter any number ");
+     
+     no=obj.nextInt();
+     System.out.println("Squre is "+(no*no));
+ }
+}
